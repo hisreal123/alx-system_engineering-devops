10164a1562f8922d8fda72c5056ef7252f245b2b
hisreal123 <omosgoerge@gmail.com>
2024-07-29 13:51:14 -0500

    task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -48,11 +48,5 @@ def get_employee_todo_progress(employee_id):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-    else:
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_todo_progress(employee_id)
-        except ValueError:
-            print("Employee ID must be an integer.")
+    get_employee_todo_progress(int(sys.argv[1]))
+
