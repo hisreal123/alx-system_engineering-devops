8d988982ebda7a8e948b4aa78528c2b8d98df662
hisreal123 <omosgoerge@gmail.com>
2024-07-30 03:06:38 -0500

    task 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,25 +7,14 @@ import requests
 import json
 
 # Base url
-BASE_URL = 'https://jsonplaceholder.typicode.com/'
-
-
-def getUser(employee_id):
-
-    try:
-        # Fetch employee data
-        userResponse = requests.get(BASE_URL + "users/{}".format(employee_id)).json()
-        todoResponse = requests.get(BASE_URL + "todos", params={"userId": sys.argv[1]}).json()
-
-        completed = [t.get("title") for t in todoResponse if t.get("completed") is True ]
-        print("Employee {} is done with tasks({}/{}):".format(
-            userResponse.get('name'), len(completed), len(todoResponse)))
-        [print("\t {}".format(c)) for c in completed]
-
-        # print(todoResponse)
-    except Exception as err:
-        print(str(err))
 
 
 if __name__ == '__main__':
-    getUser(int(sys.argv[1]))
+    BASE_URL = 'https://jsonplaceholder.typicode.com/'
+    userResponse = requests.get(BASE_URL + "users/{}".format(sys.argv[1])).json()
+    todoResponse = requests.get(BASE_URL + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [t.get("title") for t in todoResponse if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        userResponse.get('name'), len(completed), len(todoResponse)))
+    [print("\t {}".format(c)) for c in completed]
