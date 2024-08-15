5327fd0275df092e5ca46c1485cacd0f0e8de285
hisreal123 <omosgoerge@gmail.com>
2024-08-15 15:46:24 +0100

    fix 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,7 @@ def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    if response.status_code == 404:
         return 0
-
+    data = response.json().get("data")
+    return data.get('subscribers')
