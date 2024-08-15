923279094413c747242f785cd57933c7e5cadf5b
hisreal123 <omosgoerge@gmail.com>
2024-08-15 15:41:37 +0100

    fix 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ Queries the Reddit API and returns the numbes of subreddits fmor a given sub"""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    # headers =  {'User-Agent': 'custom-user-agent'}
+    headers = {'User-Agent': 'custom-user-agent'}
 
-
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception as e:
+    response = requests.get(url, headers=headers, allow_redirects=True)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
         return 0
+
