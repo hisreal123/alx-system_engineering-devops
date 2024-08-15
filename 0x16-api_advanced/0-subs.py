d5ee9b85d2f1f02bb58ba8d48aa91f3eae178803
hisreal123 <omosgoerge@gmail.com>
2024-08-15 15:48:54 +0100

    fix 0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,12 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """ returns the number of subscribers (not active users, total subscribers) for a given subreddit. """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
+    headers = {'User-Agent': 'custom-user-agent'}
+
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    data = response.json().get("data")
+    return data.get('subscribers')
